424b1120802a0a52bdf5153b1e81660d1a58159b
Davis <348915654@qq.com>
2013-01-30 00:06:37 +0800

    @ add DsResourceDislpay.cc .h

--- src/ui/DsResourceDisplay.h
+++ src/ui/DsResourceDisplay.h
@@ -2,6 +2,15 @@
 #define _DS_RESOURCE_DISPLAY_H_
 #include <QWidget>
 #include <string>
+#include <QTreeWidget>
+#include <QTreeWidgetItem>
+#include <QPushButton>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <io.h>
+#include <QDebug>
+#include <QTextCodec>
+
 class DsResourceDisplay:public QWidget 
 {
 	Q_OBJECT
@@ -14,7 +23,23 @@ class DsResourceDisplay:public QWidget
     signals:
         void resFolderDelete(const std::string& folder);
         void resFolderAdd(const std::string& floader);
-        void resFileSelect(const std::string& name);
+        void resFileSelect(const std::string& path, const std::string& name);
+
+    private:
+        QTreeWidget *m_tree;
+        QString m_dir; // 用于保存上次打开的路径
+        QString GetDirFromItem(QTreeWidgetItem *currentTreeItem); // 返回的路径末尾固定有'/'
+        QString GetParentDirFromItem(QTreeWidgetItem *currentTreeItem);
+        int AddFileItem(QString strDir, QTreeWidgetItem *currentItem); // strDir的末尾最好有'/'
+        int StrCmpByWM(const char *szstrT, const char *szstrS);
+        int CmpTypeByWM(const char *filename,const char *type);
+
+    private slots:
+        void addSomething(void);
+        void expandSomething(QTreeWidgetItem *treeItem);
+        void openSomething(void);
+        void deleteSomething(void);
+        void debugSomething(void);
 };
 
 
